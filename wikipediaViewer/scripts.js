aa3196f7acc48f50568c1cf90b80c2b823712d49
Nate Mallison <healthynate@gmail.com>
2018-03-12 22:00:33 -0400

    added more styling to tabs. working on populating data from api call

--- wikipediaViewer/scripts.js
+++ wikipediaViewer/scripts.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   const search = document.querySelector('#getQuery');
+  const tabContainer = document.querySelector('.tabLinks');
   const tabs = document.querySelectorAll('.tabLink');
   const tabContents = document.querySelectorAll('.tabContent');
 
@@ -7,7 +8,19 @@ document.addEventListener('DOMContentLoaded', function(){
     const request = new Request(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&limit=10&origin=*`);
     fetch(request).then(function(response){
       return response.json().then(function(data){
-				console.log(data);
+				// data.forEach((page, index) => {
+        //   let newTab = document.createElement('li');
+        //   newTab.classList.add('tabLink');
+        //   if (index === 0) {
+        //     newtab.classList.add('selected');
+        //   }
+        //   newTab.id = `${index}`;
+        //   newTab.textContent = page[index];
+        //   tabContainer.appendChild(newTab);
+
+        //   let newTabContent = document.createElement('div');
+
+        // })
       })
     }).catch(error => {
       console.log(error);
@@ -20,10 +33,11 @@ document.addEventListener('DOMContentLoaded', function(){
     getQuery(query);
   }
 
-  function showTab(event){
+  function showPage(event){
     const currentTab = document.querySelector('.center');
     const currentTabId = currentTab.id.substr(currentTab.id.length - 1);
-    const thisTabId = this.id;
+    const thisTabId = event.target.id;
+    console.log(thisTabId);
     if (currentTabId === thisTabId) {
       return;
     }
@@ -45,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function(){
       currentTab.classList.add('slide-right');
       thisTabContent.classList.remove('slide-left');
       tabContents.forEach((tab, index) => {
-        if ((index + 1) < currentTabId && (index+ 1) > thisTabId){
+        if ((index + 1) < currentTabId && (index + 1) > thisTabId){
           console.log(tab);
           tab.classList.add('slide-right');
           tab.classList.remove('slide-left');
@@ -55,6 +69,26 @@ document.addEventListener('DOMContentLoaded', function(){
     thisTabContent.classList.add('center');
   }
 
+  function showTab(event){
+    tabs.forEach(tab => tab.classList.remove('selected'));
+    event.target.classList.remove('hover');
+    event.target.classList.add('selected');
+  }
+
+  function showHover(event){
+    if (this.classList.contains('selected')) {
+      return;
+    } else {
+      this.classList.add('hover');
+    }
+  }
+
+  function removeHover(event){
+    if (this.classList.contains('hover')) {
+      this.classList.remove('hover');
+    }
+  }
+
   search.addEventListener('click', getInput);
   search.addEventListener('keyup', function(event){
     if (event.keyCode === 13) {
@@ -62,6 +96,12 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   }); 
 
-  tabs.forEach(tab => tab.addEventListener('click', showTab));
+  tabs.forEach(tab => tab.addEventListener('mouseenter', showHover));
+  tabs.forEach(tab => tab.addEventListener('mouseout', removeHover));
+
+  tabs.forEach(tab => tab.addEventListener('click', function(event){
+    showPage(event);
+    showTab(event);
+  }));
 
 })
