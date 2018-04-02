62c8630968e7fb4949a17c9db3756ca0eb1fd5a2
Nate Mallison <healthynate@gmail.com>
2018-04-02 08:10:05 -0400

    removed shrink-to-fit

--- pomodoroTimer/scripts.js
+++ pomodoroTimer/scripts.js
@@ -4,7 +4,20 @@ document.addEventListener('DOMContentLoaded', function(){
 
   function addToTimer(event){
     console.log(event);
-    let element = document.querySelector('#${event.id}');
+    let clickedOn = event.originalTarget.id;
+
+    if (clickedOn.includes('break')) {
+      if (clickedOn.includes('Minus')) {
+        let firstBeer = clockContainer.querySelector('img');
+        clockContainer.removeChild(firstBeer);
+      } else {
+        let newBeer = document.createElement('img');
+        newBeer.src = 'images/pint.png';
+        newBeer.setAttribute('alt', 'Pint of Beer');
+        let firstBeer = clockContainer.querySelector('img');
+        clockContainer.insertBefore(newBeer, firstBeer);
+      }
+    }
   }
 
 
@@ -13,4 +26,4 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 
-})
+});
