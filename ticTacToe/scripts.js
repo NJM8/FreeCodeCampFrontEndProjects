d749521a6b5e0a4dd0827e0dff88579484dd5683
Nate Mallison <healthynate@gmail.com>
2018-04-06 21:07:22 -0400

    basic one player functionality done. computer is very basic and only plays random available locations

--- ticTacToe/scripts.js
+++ ticTacToe/scripts.js
@@ -3,9 +3,11 @@ document.addEventListener('DOMContentLoaded', function(){
   let buttonTwo = document.querySelector('#buttonTwo');
   let display = document.querySelector('#message');
   let boardCells = document.querySelectorAll('.boardCell');
-  let whosTurn = '';
+  let lettersTurn = '';
+  let userChoice = '';
   let playing = false;
   let computerPlaying = false;
+  let cheating = false;
   let playCount = 0;
 
   let winningCombinations = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
@@ -27,8 +29,58 @@ document.addEventListener('DOMContentLoaded', function(){
     element.textContent = message;
   }
 
+  function computerPlayTurn(){
+    let options = Object.keys(board).filter(key => board[key] === 'E');
+    let selection = options[Math.floor(Math.random() * options.length)];
+    let element = document.querySelector(`[data-cell="${selection}"]`);
+    let mouseDown = new Event('mousedown');
+    setTimeout(() => {
+      cheating = false;
+      element.dispatchEvent(mouseDown);
+    }, 500);
+  }
+
+  function chooseASide(event){
+    updateElementText(display, `You chose ${event.target.textContent}`);
+    userChoice = event.target.textContent;
+    let whoGoesFirst = Math.round(Math.random());
+
+    if (whoGoesFirst === 1) {
+      lettersTurn = userChoice;
+    } else {
+      lettersTurn = ( userChoice === 'X' ? 'O' : 'X' );
+    }
+    buttonOne.disabled = true;
+    buttonTwo.disabled = true;
+    
+    setTimeout(() => {
+      updateElementText(display, `${lettersTurn}\'s turn`);
+      playing = true;
+      buttonOne.textContent = 'One Player';
+      buttonTwo.textContent = 'Two Player';
+      buttonOne.removeEventListener('mousedown', chooseASide);
+      buttonTwo.removeEventListener('mousedown', chooseASide);
+      buttonOne.addEventListener('mousedown', onePlayerGame);
+      buttonTwo.addEventListener('mousedown', twoPlayerGame);
+      if (lettersTurn !== userChoice) {
+        computerPlayTurn();
+      }
+    }, 1000);    
+  }
+
+  function onePlayerGame(){
+    computerPlaying = true;
+    updateElementText(display, 'Choose a side.');
+    buttonOne.textContent = 'X';
+    buttonTwo.textContent = 'O';
+    buttonOne.removeEventListener('mousedown', onePlayerGame);
+    buttonTwo.removeEventListener('mousedown', twoPlayerGame);
+    buttonOne.addEventListener('mousedown', chooseASide);
+    buttonTwo.addEventListener('mousedown', chooseASide);
+  }
+
   function twoPlayerGame(){
-    whosTurn = 'X';
+    lettersTurn = 'X';
     playing = true;
     updateElementText(display, 'X\'s turn');
     buttonOne.disabled = true;
@@ -36,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function(){
   }
 
   function updateBoard(cellNumber){
-    board[cellNumber] = whosTurn;
+    board[cellNumber] = lettersTurn;
   }
 
   function checkForWin(){
@@ -50,7 +102,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }
         boardCells.forEach(boardCell => {
           if (winningCombinations[i].includes(Number(boardCell.dataset.cell))) {
-            boardCell.style.color = 'firebrick';
+            boardCell.style.color = 'tomato';
           }
         });
         return true;
@@ -61,31 +113,36 @@ document.addEventListener('DOMContentLoaded', function(){
   }
 
   function playTurn(event){
+    if (cheating) {
+      return;
+    }
     if (!playing) {
       return;
     }
     if (event.target.textContent === 'O' || event.target.textContent === 'X') {
       return;
     }
-    playCount += 1;
-    updateElementText(event.target, whosTurn);
-    updateBoard(Number(event.target.dataset.cell));
 
-    if (playCount === 9) {
-      endGame(true);
-      return;
-    }
+    playCount += 1;
+    updateElementText(event.target, lettersTurn);
+    updateBoard(Number(event.target.dataset.cell));
 
     if (checkForWin()) {
       endGame();
       return;
     }
 
-    whosTurn === 'X' ? whosTurn = 'O' : whosTurn = 'X';
+    if (playCount === 9) {
+      endGame(true);
+      return;
+    }
 
-    updateElementText(display, `${whosTurn}\'s turn`);
+    lettersTurn === 'X' ? lettersTurn = 'O' : lettersTurn = 'X';
 
-    if (computerPlaying) {
+    updateElementText(display, `${lettersTurn}\'s turn`);
+
+    if (computerPlaying && lettersTurn !== userChoice) {
+      cheating = true;
       computerPlayTurn();
     }
   }
@@ -94,16 +151,19 @@ document.addEventListener('DOMContentLoaded', function(){
     if (draw) {
       updateElementText(display, 'It was a draw!');
     } else {
-      updateElementText(display, `${whosTurn} is the winner!`);
+      updateElementText(display, `${lettersTurn} is the winner!`);
     }
 
     buttonOne.disabled = false;
     buttonTwo.disabled = false;
+    lettersTurn = '';
+    userChoice = '';
     playing = false;
+    computerPlaying = false;
     playCount = 0;
     Object.keys(board).forEach(key => board[key] = 'E');
 
-    setTimeout(function(){
+    setTimeout(() => {
       boardCells.forEach(cell => {
         cell.textContent = '';
         cell.style.color = 'white';
@@ -112,7 +172,7 @@ document.addEventListener('DOMContentLoaded', function(){
     }, 2000);
   }
 
-  // buttonOne.addEventListener('mousedown', onePlayerGame);
+  buttonOne.addEventListener('mousedown', onePlayerGame);
   buttonTwo.addEventListener('mousedown', twoPlayerGame);
-  boardCells.forEach(cell => cell.addEventListener('mousedown', event => playTurn(event)));
+  boardCells.forEach(cell => cell.addEventListener('mousedown', playTurn));
 });
