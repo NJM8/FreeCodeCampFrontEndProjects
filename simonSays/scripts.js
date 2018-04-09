3de132048970a4c1ccd2e877ddb50336f3d41375
Nate Mallison <healthynate@gmail.com>
2018-04-09 12:25:33 -0400

    added simon board with basic styling

--- simonSays/scripts.js
+++ simonSays/scripts.js
@@ -7,12 +7,27 @@ document.addEventListener('DOMContentLoaded', function(){
     };
   });
 
+  function centerControls(){
+    console.log('hi');
+    const board = document.querySelector('.board');
+    const controls = document.querySelector('.controls');
+    const boardCenterY = board.getBoundingClientRect().top + (board.getBoundingClientRect().height / 2); 
+    console.log(boardCenterY);
+    const boardCenterX = board.getBoundingClientRect().left + (board.getBoundingClientRect().width / 2); 
+    console.log(boardCenterX);
+    const controlsWidth = controls.getBoundingClientRect().width;
+    console.log(controlsWidth);
+    const controlsHeight = controls.getBoundingClientRect().height;
+    console.log(controlsHeight);
+    controls.style.top = boardCenterY + (controlsHeight / 2);
+    controls.style.left = boardCenterX + (controlsWidth / 2);
+  }
 
 
 
 
 
 
-  
+  centerControls();
 });
 
